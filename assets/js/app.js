2a02e1c9e653440c7e58f4dfa6ebca41a8c362b8
vaishnavi Deoke <vaishnavideoke1411@gmail.com>
2026-05-18 13:49:23 +0530

    Wok on Submit Functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,12 +14,13 @@ let todoArr = [
 
 ]; 
 
-
+const todoForm = document.getElementById('todoForm')
+const todoItemContainer = document.getElementById('todoItem')
 const todoContainer =document.getElementById('todoContainer');
 
 
 function template(arr){
-  let res = ' '; 
+  let res = ' ' ;
 
   arr.forEach(ele=>{  
     res +=`<li id="${ele.todoId}" class="list-group-item d-flex justify-content-between">
@@ -35,3 +36,40 @@ function template(arr){
 }
 
 template(todoArr)
+
+function onTodoSubmit(eve) {
+
+
+  eve.preventDefault()
+  // cl('Submitted...')
+  let newTodo ={
+    todoItem : todoItemContainer.value ,
+    todoId :  Date.now().toString()
+  }
+  // 4cl(newTodo)T
+  todoArr.push(newTodo);
+
+  let li = document.createElement('li');
+  li.className = 'list-group-item d-flex justify-content-between';
+  li.id = newTodo.todoId;
+
+  li.innerHTML = `
+        <strong>${newTodo.todoItem}</strong>
+          <div>
+              <i class="fa-solid fa-pen-to-square  text-primary"  ></i>
+              <i class="fa-solid fa-trash-can   text-danger"  ></i>
+          </div>
+          `;
+
+          todoContainer.append(li)
+          
+      swal.fire({
+        title : `The new Todo item ${newTodo.todoItem} item added successfull...!`,
+        timer : 3000,
+        icon : 'success'
+
+   })
+}
+
+
+todoForm.addEventListener('submit',onTodoSubmit)
